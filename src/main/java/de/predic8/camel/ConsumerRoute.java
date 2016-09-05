69ba520fbcc4637976d87174036ab52195355683
Thomas Bayer <bayer@predic8.de>
2016-09-05 20:28:51 +0200

    Anpassungen fuer Teil 6: Camel und ActiveMQ

--- src/main/java/de/predic8/camel/ConsumerRoute.java
+++ src/main/java/de/predic8/camel/ConsumerRoute.java
@@ -8,14 +8,17 @@ public class ConsumerRoute extends RouteBuilder {
     @Override
     public void configure() throws Exception {
 
+        from("file:in")
+                .id("file-in")
+                .log("Nachricht: ${body}");
+
+        /* Für Teil #6 Camel & ActiveMQ
+           bitte Kommentar entfernen
         from("activemq:in")
                 .id("queue-in")
-                .log("Nachricht empfangen: ${body}")
-                .to("activemq:bereit");
-
-        from("activemq:bereit")
                 .log("Nachricht verarbeitet: ${body}")
                 .to("file:processed");
+        */
 
     }
 }
